56c78a8485cb3a04cb1cf7697be670a5316b5e6f
Alok Patel <alok@agileware.com.au>
2018-08-13 12:44:24 +0530

    SPA-263: Prevent Sending copy to assignee contacts from Case if particular activity type is excluded.

--- CRM/Case/Form/Activity.php
+++ CRM/Case/Form/Activity.php
@@ -620,7 +620,10 @@ class CRM_Case_Form_Activity extends CRM_Activity_Form_Activity {
     //CRM-5695
     //check for notification settings for assignee contacts
     $selectedContacts = ['contact_check'];
-    if (Civi::settings()->get('activity_assignee_notification')) {
+    $activityContacts = CRM_Activity_BAO_ActivityContact::buildOptions('record_type_id', 'validate');
+    $assigneeID = CRM_Utils_Array::key('Activity Assignees', $activityContacts);
+
+    if (Civi::settings()->get('activity_assignee_notification') && !in_array($params['activity_type_id'], Civi::settings()->get('do_not_notify_assignees_for'))) {
       $selectedContacts[] = 'assignee_contact_id';
     }
 
