58b923e71ff2891a3421ace2e0b5c20d9e0922fb
Justin Freeman <justin@agileware.com.au>
2023-07-06 13:25:53 +1000

    CIVIBLD-238 Disable the valid path check for the wkhtmltopdf executable

--- CRM/Admin/Form/Setting/Miscellaneous.php
+++ CRM/Admin/Form/Setting/Miscellaneous.php
@@ -115,20 +115,6 @@ class CRM_Admin_Form_Setting_Miscellaneous extends CRM_Admin_Form_Setting {
       $errors['recentItemsMaxCount'] = ts("Illegal stack size. Use values between 1 and %1.", [1 => CRM_Utils_Recent::MAX_ITEMS]);
     }
 
-    if (!empty($fields['wkhtmltopdfPath'])) {
-      // check and ensure that this path leads to the wkhtmltopdf binary
-      // and it is a valid executable binary
-      // Only check the first space separated piece to allow for a value
-      // such as /usr/bin/xvfb-run -- wkhtmltopdf (CRM-13292)
-      $pieces = explode(' ', $fields['wkhtmltopdfPath']);
-      $path = $pieces[0];
-      if (
-        !file_exists($path) ||
-        !is_executable($path)
-      ) {
-        $errors['wkhtmltopdfPath'] = ts('The wkhtmltodfPath does not exist or is not valid');
-      }
-    }
     return $errors;
   }
 
