4468c058e05e65a3ef02af16d3a7528a1b4f4342
Justin Freeman <justin@agileware.com.au>
2023-10-06 09:51:48 +1100

    CIVIBLD-241 Re-roll patch. Hardcode disabling the "Accept profile submissions from external sites" feature in System Settings, potential security issue

--- CRM/UF/Page/Group.php
+++ CRM/UF/Page/Group.php
@@ -116,7 +116,7 @@ class CRM_UF_Page_Group extends CRM_Core_Page {
           'weight' => CRM_Core_Action::getWeight(CRM_Core_Action::COPY),
         ],
       ];
-      $allowRemoteSubmit = Civi::settings()->get('remote_profile_submissions');
+      $allowRemoteSubmit = FALSE;
       if ($allowRemoteSubmit) {
         self::$_actionLinks[CRM_Core_Action::PROFILE] = [
           'name' => ts('HTML Form Snippet'),
